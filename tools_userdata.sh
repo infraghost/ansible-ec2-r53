aae5f0599dc69cb8d37f4ffe808c6eee680c1d12
infraghost <hemasivakishore99@gmail.com>
2025-07-16 18:10:41 +0530

    userdata testing pending

--- tools_userdata.sh
+++ tools_userdata.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#exec > /var/log/userdata.log 2>&1
+set -eux
+
 sudo dnf update -y
-sudo dnf install -y epel-release
 sudo dnf install -y python3 python3-pip
 sudo pip3 install --upgrade pip
 sudo pip3 install boto boto3 botocore
-sudo dnf install nano ansible -y 
+sudo dnf install -y nano ansible nginx
+
+sudo systemctl enable nginx
+sudo systemctl start nginx
